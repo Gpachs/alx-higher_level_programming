197e40035a2332cab598e0633e47dd00a450131c
Gpachs <obiomagoldman@gmail.com>
2022-08-28 11:01:31 -0700

    python code that adds integegers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total = len(sys.argv) - 1
+    for i in range(total):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
