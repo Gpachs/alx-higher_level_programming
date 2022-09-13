82f491158c779a30a1474b4b9d1740e773074631
Gpachs <obiomagoldman@gmail.com>
2022-09-13 09:39:38 -0700

    python exceptions

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    try:
+        div_result = int(list_length) / 2
+    except ZeroDivisionError:
+        div_result = 0
+    finally:
+        return list_length
