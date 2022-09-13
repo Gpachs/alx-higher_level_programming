65fcc34b304c2b3e5c20796a809a287e00338f90
Gpachs <104031141+Gpachs@users.noreply.github.com>
2022-09-13 22:44:32 +0100

    Update 5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise TypeError("Wrong typing, try agin please!")
-    except TypeError as e:
-        print(e)
+    #try:
+    raise TypeError#("Wrong typing, try agin please!")
+    #except TypeError as e:
+        #print(e)
