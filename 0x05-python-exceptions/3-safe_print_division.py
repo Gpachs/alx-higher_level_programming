6f99ac93158cea34175852ca87a100542ec8a0e7
Gpachs <obiomagoldman@gmail.com>
2022-09-12 17:14:06 -0700

    exceptions in python

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         divide = a / b
     except Exception:
-        return divide
+        return None
     finally:
         print("Inside result: {}".format(divide))
-        return None
+    return divide
