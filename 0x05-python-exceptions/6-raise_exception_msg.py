c39b8c36fd073cc37efe524d187dae61e6c65918
Gpachs <104031141+Gpachs@users.noreply.github.com>
2022-09-13 22:59:19 +0100

    Update 6-raise_exception_msg.py

--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-  raise NameError("Sorry, The name Is Incorrect.. Try Again Please")
+  try:
+    raise NameError("Sorry, The name Is Incorrect.. Try Again Please")
+  except NameError as e:
+    print(e)
