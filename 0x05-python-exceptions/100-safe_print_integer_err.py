77e8a503025cfe940fc9dde1c237ca0492de036c
Gpachs <104031141+Gpachs@users.noreply.github.com>
2022-09-14 11:31:22 +0100

    Update 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
-        print("fatal error", file=sys.stderr)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return False
